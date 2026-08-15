cd71c07be71988d477b9e76cdc7b54dd65b05c7a
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-15 15:09:50 +0300

    set default value for movie

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,7 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = ({ movies }) => (
+export const MovieList = ({ movies = [] }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
